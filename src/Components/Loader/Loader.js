b38c405c182074ca4ae3a653309487e76dceec6a
Fantom1416 <barnoyusupova102@example.com>
2023-02-20 01:36:10 -0800

    first commit

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -3,7 +3,7 @@ import "./Loader.css"
 
 function Loader() {
     return (
-        <div className='obshiy'>
+        <div className='all'>
             <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
         </div >
     )
